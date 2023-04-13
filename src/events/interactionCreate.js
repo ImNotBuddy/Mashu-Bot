85638c4e8de6c5dee51a95d1e9759547c62a1041
ImNotBuddy <jaikdhir@gmail.com>
2023-04-13 17:01:51 +0100

    new file:   src/commands/tools/reactroles.js 	modified:   src/events/interactionCreate.js 	modified:   src/index.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,22 +1,55 @@
-const { Events } = require('discord.js');
+const { Events, Interaction } = require('discord.js');
 
 module.exports = {
 	name: Events.InteractionCreate,
+
+	/**
+	 * 
+	 * @param {Interaction} interaction 
+	 * @returns 
+	 */
+
 	async execute(interaction) {
-		if (!interaction.isChatInputCommand()) return;
 
-		const command = interaction.client.commands.get(interaction.commandName);
+		if (interaction.isChatInputCommand()) {
+			const command = interaction.client.commands.get(interaction.commandName);
 
-		if (!command) {
-			console.error(`No command matching ${interaction.commandName} was found.`);
+			if (!command) {
+				console.error(`No command matching ${interaction.commandName} was found.`);
+				return;
+			}
+
+			try {
+				await command.execute(interaction);
+			} catch (error) {
+				console.error(`Error executing ${interaction.commandName}`);
+				console.error(error);
+			}
+		} else if (interaction.isButton()) {
+			try {
+				await interaction.deferReply({ ephemeral: true });
+
+				const role = interaction.guild.roles.cache.get(interaction.customId);
+				if (!role) {
+					interaction.editReply({
+						content: "Could not find that role",
+					});
+					return;
+				}
+
+				const hasRole = interaction.member.roles.cache.has(role.id);
+				if (hasRole) {
+					await interaction.member.roles.remove(role);
+					await interaction.editReply(`The role ${role} was removed`);
+				} else {
+					await interaction.member.roles.add(role);
+					await interaction.editReply(`The role ${role} was added`);
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		} else {
 			return;
-		}
-
-		try {
-			await command.execute(interaction);
-		} catch (error) {
-			console.error(`Error executing ${interaction.commandName}`);
-			console.error(error);
 		}
 	},
 }
